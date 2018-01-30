d93bb17a4eb0ceb55e1c2248da9e78f781fa45b0
ning <egg11272001@gmail.com>
2018-01-29 20:33:34 -0800

    add shot chart

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { TopNavBar } from './TopNavBar';
+import { Main } from './Main';
 import '../styles/App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <TopNavBar/>
+          <Main/>
       </div>
     );
   }

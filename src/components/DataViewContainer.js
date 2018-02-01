b0d2fc12711e67ff704c17fce56f59697d9d7b80
ning <egg11272001@gmail.com>
2018-01-31 20:33:14 -0800

    slider integration

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -3,15 +3,22 @@ import { ShotChart } from './ShotChart';
 import { CountSlider} from './CountSlider'
 
 export class DataViewContainer extends React.Component {
+    state= {
+        minCount: 2,
+    }
+
+    onCountSliderChange = (count) => {
+        this.setState({ minCount: count });
+    }
 
     render() {
         return (
             <div className="data-view">
-                <ShotChart playerId={this.props.playerId}/>
+                <ShotChart playerId={this.props.playerId} minCount={this.state.minCount}/>
                 <div className="filters">
-                    <CountSlider/>
+                    <CountSlider onCountSliderChange={this.onCountSliderChange}/>
                 </div>
             </div>
-        )
+        );
     }
 }
